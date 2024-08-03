af6410389e4fa45714ed3583d1abb040d2b4900f
abyanardiatama <abyanardiatama@students.undip.ac.id>
2024-08-04 06:31:59 +0700

    build board

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 export default function Square(){
     return (
         <>
-        <button clasName="square">X</button>
-        <button clasName="square">X</button>
-        <button clasName="square">X</button>
-        <button clasName="square">X</button>
-        <button clasName="square">X</button>
-        <button clasName="square">X</button>
-        <button clasName="square">X</button>
-        <button clasName="square">X</button>
-        <button clasName="square">X</button>
+        <div className="board-row">
+            <button clasName="square">1</button>
+            <button clasName="square">2</button>
+            <button clasName="square">3</button>
+        </div>
+        <div className="board-row">
+            <button clasName="square">4</button>
+            <button clasName="square">5</button>
+            <button clasName="square">6</button>
+        </div>
+        <div className="board-row">
+            <button clasName="square">7</button>
+            <button clasName="square">8</button>
+            <button clasName="square">9</button>
+        </div>
         </>
     );
 }
